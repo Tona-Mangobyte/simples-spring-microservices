dcb375a6bfa44a0b91852b069a60a94012eeab7c
Tona-Mangobyte <tona@mango-byte.com>
2023-07-02 15:14:20 +0700

    init svr

---
+++ s1-service/src/main/java/com/example/s1service/S1ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.s1service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S1ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S1ServiceApplication.class, args);
+    }
+
+}
---
+++ s2-service/src/main/java/com/example/s2service/S2ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.s2service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S2ServiceApplication.class, args);
+    }
+
+}
